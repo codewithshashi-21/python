40ed7cc8711d98474614edd1ecfea55f8fe4053a
codewithshashi-21 <shashivardhan93906@gmail.com>
2025-10-02 07:38:24 +0530

    Added Day 1 programs

---
+++ day1.py
@@ -0,0 +1,29 @@
+#program1 
+print("Hello Sunny🐼, welcome to Python!")
+#program2
+name = "Sunny"
+age = 21
+height = 5.4
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+a = 10
+b = 20
+print("Before swap: a =", a, "b =", b)
+a, b = b, a
+print("After swap: a =", a, "b =", b)
+
+num1 = 15
+num2 = 5
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+x = 10
+y = 12.5
+z = "Sunny"
+print(type(x))
+print(type(y))
+print(type(z))
